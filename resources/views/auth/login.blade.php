bcaa9faf981659cc17deffcd95a66298d21c7e9f
Josue Sosa <josue.sosa@hotmail.es>
2019-04-21 19:19:01 -0600

    avances en formulario

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="user_name" class="col-md-4 col-form-label text-md-right">{{ __('User') }}</label>
+                            <label for="user_name" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
 
                             <div class="col-md-6">
                                 <input id="user_name" type="text" class="form-control{{ $errors->has('user_name') ? ' is-invalid' : '' }}" name="user_name" value="{{ old('user_name') }}" required autocomplete="off" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="off">
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recordarme') }}
                                     </label>
                                 </div>
                             </div>
@@ -57,11 +57,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('¿Olvidó su contraseña?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
@@ -70,22 +70,4 @@
         </div>
     </div>
 </div>
-  {{-- <div class="login-dark">
-    <form method="post" action="{{ route('login') }}">
-      <div class="illustration"><i class="fas fa-id-card-alt"></i></div>
-      <div class="form-group"><input class="form-control" type="text" name="user_name" placeholder="Usuario"></div>
-      <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Contraseña"></div>
-      <div class="form-group row mb-0">
-          <div class="col-md-6 offset-md-4">
-              <button type="submit" class="btn btn-primary">
-                  {{ __('Register') }}
-              </button>
-          </div>
-      </div>
-      <a class="forgot" href="#"></a>
-    </form>
-  </div> --}}
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +19,50 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col"></div>
+      <div class="col-8">
+        <!--Busqueda-->
+        <div class="row">
+          <div class="col-md-12">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Buscar paciente">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Buscar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--Formulario-->
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" name="test" placeholder="test" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" name="test" placeholder="test2" class="form-control">
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col"></div>
+          <div class="col-md-8">
+            <div class="form-group text-center">
+              <button type="submit" class="btn btn-primary">Guardar expediente</button>
+            </div>
+          </div>
+          <div class="col"></div>
+        </div>
+
+
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
 @endsection
