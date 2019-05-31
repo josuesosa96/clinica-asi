2f7572fae76c8fa01e7d9e90d6f7290173ec35c9
Josue Sosa <josue.sosa@hotmail.es>
2019-05-31 17:29:35 -0600

    app.yaml

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'clinica'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '30enero96'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'password' => env('DB_PASSWORD', 'clinicaasi'),
+            'unix_socket' => env('DB_SOCKET', '/cloudsql/clinica-asi1:us-central1:clinica'),
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
             'prefix' => '',
